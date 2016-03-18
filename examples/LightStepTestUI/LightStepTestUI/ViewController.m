df603984b97f2a2b4aad53a68145f46116c73d6d
bcronin <bcronin@resonancelabs.com>
2016-03-18 16:41:00 -0700

    Add GitHub API info

--- examples/LightStepTestUI/LightStepTestUI/ViewController.m
+++ examples/LightStepTestUI/LightStepTestUI/ViewController.m
@@ -35,7 +35,40 @@
     [alert show];
 
     [NSThread sleepForTimeInterval:.05];
+    NSDictionary* user = [self _getHTTP];
+
+    NSMutableString* output = [NSMutableString new];
+    [output appendString:[NSString stringWithFormat:@"User: %@\n", user[@"login"]]];
+    [output appendString:[NSString stringWithFormat:@"Type: %@\n", user[@"type"]]];
+
+    NSLog(@"%@", output);
 
     [span finish];
 }
+
+- (NSDictionary*)_getHTTP {
+
+    NSURL* url = [NSURL URLWithString:@"https://api.github.com/users/lightstep"];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url
+                                                           cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+                                                       timeoutInterval:10];
+    [request setHTTPMethod: @"GET"];
+
+    NSMutableDictionary* headers = [NSMutableDictionary dictionaryWithDictionary:[request allHTTPHeaderFields]];
+    [headers setObject:@"LightStep iOS Example" forKey:@"User-Agent"];
+    request.allHTTPHeaderFields = headers;
+
+    NSError *requestError = nil;
+    NSURLResponse *urlResponse = nil;
+    NSData *response = [NSURLConnection sendSynchronousRequest:request
+                                              returningResponse:&urlResponse
+                                                          error:&requestError];
+    NSString *jsonString = [[NSString alloc] initWithData:response
+                                                 encoding:NSUTF8StringEncoding];
+
+    NSMutableDictionary *result = [NSJSONSerialization JSONObjectWithData:response
+                                                             options:NSJSONReadingMutableContainers error:nil];
+    return result;
+}
+
 @end
