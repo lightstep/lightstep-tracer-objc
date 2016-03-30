cec273a9188a6ce6cd59c8a46fc1f9ba9ff582eb
Ben Sigelman <bhs@resonancelabs.com>
2016-03-29 19:21:52 -0700

    Bring thrift in so we can swift-bridge it
    Make some other minor adjustments.

---
+++ Pod/Classes/LightStep-Bridging-Header.h
@@ -0,0 +1,43 @@
+//
+//  LightStep-Bridging-Header.h
+//  Pods
+//
+
+#ifndef LightStep_Bridging_Header_h
+#define LightStep_Bridging_Header_h
+
+#import "LSClockState.h"
+#import "LSSpan.h"
+#import "LSTracer.h"
+#import "LSUtil.h"
+#import "LSVersion.h"
+#import "LightStep-Bridging-Header.h"
+#import "TApplicationException.h"
+#import "TBase.h"
+#import "TBinaryProtocol.h"
+#import "TException.h"
+#import "TFramedTransport.h"
+#import "THTTPClient.h"
+#import "TMemoryBuffer.h"
+#import "TMultiplexedProtocol.h"
+#import "TNSFileHandleTransport.h"
+#import "TNSStreamTransport.h"
+#import "TObjective-C.h"
+#import "TProcessor.h"
+#import "TProcessorFactory.h"
+#import "TProtocol.h"
+#import "TProtocolDecorator.h"
+#import "TProtocolException.h"
+#import "TProtocolFactory.h"
+#import "TProtocolUtil.h"
+#import "TSSLSocketClient.h"
+#import "TSSLSocketException.h"
+#import "TSharedProcessorFactory.h"
+#import "TSocketClient.h"
+#import "TSocketServer.h"
+#import "TTransport.h"
+#import "TTransportException.h"
+#import "Thrift.h"
+#import "crouton.h"
+
+#endif /* LightStep_Bridging_Header_h */
