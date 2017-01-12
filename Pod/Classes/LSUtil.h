e3f2ac0b77a29302eae54e211b6cbc8146e09aa5
Marin Usalj <marin2211@gmail.com>
2017-01-12 12:14:18 -0800

    objc

--- Pod/Classes/LSUtil.h
+++ Pod/Classes/LSUtil.h
@@ -4,21 +4,20 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class GPBTimestamp;
 
-/**
- * Shared, generic utility functions used across the library.
- */
-@interface LSUtil : NSObject
+
+/// Shared, generic utility functions used across the library.
+@interface LSUtil: NSObject
 
 + (UInt64)generateGUID;
-+ (NSString*)hexGUID:(UInt64)guid;
-+ (UInt64)guidFromHex:(NSString*)hexString;
-+ (NSString*)objectToJSONString:(nullable id)obj maxLength:(NSUInteger)maxLength;
-+ (NSMutableArray*)keyValueArrayFromDictionary:(NSDictionary<NSString*, NSObject*>*)dict;
++ (NSString *)hexGUID:(UInt64)guid;
++ (UInt64)guidFromHex:(NSString *)hexString;
++ (NSString *)objectToJSONString:(nullable id)obj maxLength:(NSUInteger)maxLength;
++ (NSMutableArray *)keyValueArrayFromDictionary:(NSDictionary<NSString *, NSObject *> *)dict;
 
 @end
 
 @interface NSDate (LSSpan)
-- (int64_t) toMicros;
+- (int64_t)toMicros;
 @end
 
 NS_ASSUME_NONNULL_END
--- Pod/Classes/LSUtil.m
+++ Pod/Classes/LSUtil.m
@@ -7,11 +7,11 @@
     return (((UInt64)arc4random()) << 32) | arc4random();
 }
 
-+ (NSString*)hexGUID:(UInt64)guid {
++ (NSString *)hexGUID:(UInt64)guid {
     return [NSString stringWithFormat:@"%llx", guid];
 }
 
-+ (UInt64)guidFromHex:(NSString*)hexString {
++ (UInt64)guidFromHex:(NSString *)hexString {
     NSScanner *scanner = [NSScanner scannerWithString:hexString];
     UInt64 rval;
     if (![scanner scanHexLongLong:&rval]) {
@@ -20,22 +20,24 @@
     return rval;
 }
 
-+ (NSString*)objectToJSONString:(id)obj
-                      maxLength:(NSUInteger)maxLength {
-    NSString* json = [LSUtil _objectToJSONString:obj];
-    if ([json length] > maxLength) {
-        NSLog(@"Dropping excessively large payload: length=%@", @([json length]));
-        json = nil;
++ (NSString*)objectToJSONString:(id)obj maxLength:(NSUInteger)maxLength {
+    NSString *json = [LSUtil objectToJSONString:obj];
+    if (json.length > maxLength) {
+        NSLog(@"Dropping excessively large payload: length=%@", @(json.length));
+        return nil;
     }
     return json;
 }
 
+
+#pragma mark - Private
+
 // Convert the object to JSON without string length constraints
-+ (NSString*)_objectToJSONString:(id)obj {
++ (NSString *)objectToJSONString:(id)obj {
     if (obj == nil) {
         return nil;
     } else if ([NSJSONSerialization isValidJSONObject:obj]) {
-        return [LSUtil _serializeToJSON:obj];
+        return [LSUtil serializeToJSON:obj];
     } else {
         // To avoid reinventing encoding logic, reuse NSJSONSerialization
         // for basic value-types via a temporary dictionary and slicing out the
@@ -44,14 +46,14 @@
         // Due to the nature of JSON and the assumption that NSJSONSerialization
         // will *not* inject unnecessary whitespace, the position of the encoded
         // object is fixed.
-        NSString* output = [LSUtil _serializeToJSON:@{@"V":obj}];
+        NSString *output = [LSUtil serializeToJSON:@{@"V":obj}];
         if (output == nil) {
             return output;
         }
         if ([output length] < 6) {
             NSLog(@"NSJSONSerialization generated invalid JSON: %@", output);
         }
-        NSString* trimmed = [output stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        NSString *trimmed = [output stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
         if (![trimmed hasPrefix:@"{\"V\":"] || ![trimmed hasSuffix:@"}"]) {
             NSLog(@"Unexpected JSON encoding: %@", output);
             return nil;
@@ -60,12 +62,12 @@
     }
 }
 
-+ (NSString*)_serializeToJSON:(id)dict {
-    NSError* error;
-    NSData* jsonData;
++ (NSString *)serializeToJSON:(NSDictionary *)dict {
+    NSError *error;
+    NSData *jsonData;
     @try {
         jsonData = [NSJSONSerialization dataWithJSONObject:dict options:0 error:&error];
-    } @catch (NSException* e) {
+    } @catch (NSException *e) {
         NSLog(@"Invalid object for JSON conversation");
         return nil;
     }
@@ -76,14 +78,11 @@
     return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
 }
 
-+ (NSMutableArray*)keyValueArrayFromDictionary:(NSDictionary<NSString*, NSObject*>*)dict {
-    NSMutableArray* rval = [NSMutableArray arrayWithCapacity:dict.count];
-    for (NSString* key in dict) {
-        NSObject* val = dict[key];
-        [rval addObject:@{@"Key": key,
-                          @"Value": ([val isKindOfClass:[NSString class]]
-                                     ? val
-                                     : val.description)}];
++ (NSMutableArray *)keyValueArrayFromDictionary:(NSDictionary<NSString *, NSObject *> *)dict {
+    NSMutableArray *rval = [NSMutableArray arrayWithCapacity:dict.count];
+    for (NSString *key in dict) {
+        NSObject *val = dict[key];
+        [rval addObject:@{@"Key": key, @"Value": val.description}];
     }
     return rval;
 }
@@ -91,9 +90,7 @@
 @end
 
 @implementation NSDate(LSSpan)
-- (int64_t) toMicros {
+- (int64_t)toMicros {
     return (int64_t)([self timeIntervalSince1970] * USEC_PER_SEC);
 }
 @end
-
-
