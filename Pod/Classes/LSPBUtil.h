9da46f61c9d433e985491c5497b92ab34bbb9dff
Joe Blubaugh <cblubaugh@twitter.com>
2017-03-30 17:06:36 -0700

    Better docs in PBUtil header.

--- Pod/Classes/LSPBUtil.h
+++ Pod/Classes/LSPBUtil.h
@@ -25,12 +25,26 @@ typedef struct _keyInfo {
 
 @interface LSPBUtil : NSObject
 
+// Reads a fixed-width encoding of 64 bit integers from the data, advancing offset at the same time.
 + (UInt64)readLittleEndianUInt64From:(NSData *)data offset:(UInt64 *)offset;
+
+// Reads the field's tag value and length from the data, advancing the offset at the same time.
 + (LSPBKeyInfo)nextKeyFromProto:(NSData *)protoEnc offset:(UInt64 *)offset;
+
+// Reads a varint-encoded integer from the data, advancing the offset at the same time.
 + (UInt64)readVarintFromProto:(NSData *)proto startingAt:(UInt64 *)offset;
+
+// Writes the tag number and field format into the buffer as a varint.
+// @see https://developers.google.com/protocol-buffers/docs/encoding#structure
 + (void)writeTagNumber:(UInt64)tagNumber format:(Byte)format buffer:(NSMutableData *)buffer;
+
+// Writes a NSDictionary containing only NSString * keys and values as a repeated entry to the buffer.
 + (void)writeStringMap:(NSDictionary *)data tagNumber:(UInt64)tagNumber buffer:(NSMutableData *)buffer;
+
+// Writes a UInt64 in the fixed, little-endian encoding to the buffer.
 + (void)writeFixedEncodedUInt64:(UInt64)number buffer:(NSMutableData *)buffer;
+
+// Writes a UInt64 in the varint encoding to the buffer.
 + (void)writeVarintEncodedUInt64:(UInt64)number buffer:(NSMutableData *)buffer;
 
 @end
