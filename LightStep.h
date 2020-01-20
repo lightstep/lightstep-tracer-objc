04d5748c5876d8b5914f69718ae78032eb590fab
Osama Bin Bashir <osama.bashir@zalando.de>
2020-01-20 10:34:45 +0100

    New files added for making it a Cart

---
+++ LightStep.h
@@ -0,0 +1,24 @@
+//
+//  LightStep.h
+//  LightStep
+//
+//  Created by Osama Bashir on 1/17/20.
+//  Copyright © 2020 LightStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightStep.
+FOUNDATION_EXPORT double LightStepVersionNumber;
+
+//! Project version string for LightStep.
+FOUNDATION_EXPORT const unsigned char LightStepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightStep/PublicHeader.h>
+
+#import <LightStep/LSClockState.h>
+#import <LightStep/LSSpan.h>
+#import <LightStep/LSSpanContext.h>
+#import <LightStep/LSTracer.h>
+#import <LightStep/LSUtil.h>
+#import <LightStep/LSVersion.h>
